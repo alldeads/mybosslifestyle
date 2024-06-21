699301d5254218dcdd0b9749f735fde97e6f4cd5
John F <johnrexter.flores@gmail.com>
2024-06-21 22:01:57 +0800

    Add product resource

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->title(),
+            'bio' => fake()->text(50),
+            'description' => fake()->text(200),
+            'price' => rand(100,200),
+            'quantity' => rand(1000, 2000),
+        ];
+    }
+}
