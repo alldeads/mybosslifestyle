e88856b4d67d70620865283304c1efce567e55f1
John F <johnrexter.flores@gmail.com>
2024-07-22 01:21:39 +0800

    Added account number

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -61,7 +61,11 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->account_number }}</div>
+                            <div>
+                                {{ Auth::user()->username }}<br>
+                                <span>{{ Auth::user()->account_number }}</span>
+                            </div>
+
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
