507466ebc4b50495e8c4a68c22669c04b00cc7e7
John F <johnrexter.flores@gmail.com>
2024-07-27 14:09:17 +0800

    Fix issue on dashboard

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,30 +2,32 @@
 
 namespace App\Filament\Widgets;
 
+use Carbon\Carbon;
 use App\Models\Product;
 use App\Models\User;
+use App\Models\Transaction;
+use App\Models\Redeem;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Card;
 
 class StatsOverview extends BaseWidget
 {
-    protected function getStats(): array
+    protected function getCards(): array
     {
         $users = User::where('is_admin', false)->count();
-        // $transactions = Transaction::where('status', 'paid')->count();
+        $transactions = Transaction::where('status', 'paid')->count();
         $products = Product::count();
+        $redeems = Redeem::where('status', 'pending')->count();
+
+        // Stats
+        $todayPoints = Transaction::where('created_at', '>=', Carbon::today())->sum('points');
 
         return [
             Card::make('Customers', $users),
-            Card::make('Transactions', 0),
+            Card::make('Transactions', $transactions),
             Card::make('Products', $products),
-        ];
-    }
-
-    protected function getCards(): array
-    {
-        return [
-
+            Card::make('Pending Redeems', $redeems),
+            Card::make('Today\'s Points', $todayPoints),
         ];
     }
 }
